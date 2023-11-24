b1274ce504d5453f3e14b5712e57663928472b44
Nishant <vikaskumar.e1256@gmail.com>
2023-11-24 16:06:34 +0530

    migration added for post, tag, categories

---
+++ database/migrations/2023_11_24_102458_create_category_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_posts', function (Blueprint $table) {
+            $table->foreignIdFor(Post::class)->onDelete('cascade');;
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_posts');
+    }
+};
---
+++ database/migrations/2023_11_24_103124_create_post_tags_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Tag;
+use App\Models\Post;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->foreignIdFor(Post::class)->onDelete('cascade');;
+            $table->foreignIdFor(Tag::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tags');
+    }
+};
